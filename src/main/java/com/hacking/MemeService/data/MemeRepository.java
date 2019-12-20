1d8a5b0fa8534981ade57cf8ef6794d6b7562c7c
kochris <chris.ko791@gmail.com>
2019-12-19 23:37:55 -0500

    Deployment #0

--- src/main/java/com/hacking/MemeService/data/MemeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hacking.MemeService.data;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface MemeRepository extends MongoRepository<Meme, String> {
-
-    public Meme findByTitle(String title);
-
-}
